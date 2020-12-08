a109270b3453e0da68c20a31c5fe106fa9a5b4c0
Daniele Esposito <esp.dne@gmail.com>
2020-12-08 21:59:48 +0100

    start again

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-require("dotenv/config");
-
-//Middlewares
-
-app.use("/posts", ()=>{
-    console.log("this is a middleware running")
-})
+const app = expresse();
 
 //ROUTES
-
-app.get("/",(req,res)=>{
-    res.send("we are on home");
+app.get("/", (req, res) => {
+  res.send("WE ARE ON HOME");
 });
 
-app.get("/posts",(req,res)=>{
-    res.send("we are on posts");
-});
-
-//Connect to DB
-mongoose.connect(
-    process.env.DB_CONNECTION,
-{ useNewUrlParser: true },
- ()=>{
-    console.log("Connected to DB");
-})
-
-//listen to the server
-app.listen(2500);
+//Listen to the Server
+app.listen(3000);
